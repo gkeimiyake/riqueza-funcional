5a7f4a9b19f884f3d64b8795508cd4002feb9738
gkeimiyake <83677592+gkeimiyake@users.noreply.github.com>
2024-10-02 17:23:03 -0300

    Update espaco funcional.R

--- espaco funcional.R
+++ espaco funcional.R
@@ -1,30 +1,36 @@
-##biocampo economia foliar DF
 library("FD")
-library("readxl")
-
-
-##script prof
-
-
 read.table("matriz_comunidade.tsv", header=T)->com
 read.table("atributos_espécie.tsv", header=T)->traits
-dbFD(traits,com)->res
-res
-
-### zero-sum abundances 
-
-
+##ajuste das matrizes para realizar dbFD
 zero_sum_rows <- rowSums(com) == 0
 which(zero_sum_rows)
-com_filtered <- com[!zero_sum_rows, ]
-rowSums(com_filtered)
-
-
-fd_result <- dbFD(traits, com_filtered)
-dbFD(traits, com_filtered, corr="lingoes")->res
-
+com_filtrado <- com[!zero_sum_rows, ]
+rowSums(com_filtrado)
+## dbFD
+fd_result <- dbFD(traits, com_filtrado)
+dbFD(traits, com_filtrado, corr="lingoes")->res
 names(res)
 res$FRic
 res$nbsp
-plot(res$nbsp,res$FRic)
-
+##plotagem FRic por nbsp
+plot(res$nbsp,res$FRic,
+     xlab = "Número de Espécies (nbsp)",
+     ylab = "Riqueza Funcional (FRic)",
+     main = "Relação entre Número de espécies e Riqueza Funcional")
+text(res$nbsp, res$FRic, labels=rownames(com_filtrado), pos=4, cex=1)
+##teste de regressão 
+regressao<-lm(res$FRic~res$nbsp)
+summary(regressao)
+abline(regressao)
+regressao
+##teste de correlação
+correlacao<-cor.test(res$nbsp,res$FRic)
+print(correlacao)
+##verificação de resíduos 
+residuos<-regressao$residuals
+qqnorm(residuos)
+qqline(residuos, col="blue")
+histogram(residuos)
+summary(residuos)
+teste_normalidade<-shapiro.test(residuos)
+print(teste_normalidade)
